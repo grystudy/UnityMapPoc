84a686e67ec0ac1ed9296017ed303328e9f74b56
yy <yangyu@mxnavi.com>
2022-04-01 14:18:48 +0800

    cjson parse roads

--- mx_engine/test/mx_dll/mx_dll.cpp
+++ mx_engine/test/mx_dll/mx_dll.cpp
@@ -4,7 +4,9 @@
 #include "mx_dll.h"
 
 #include <windows.h>
+#include "cjson/cJSON.h"
 
+#include<vector>
 
 //[DllImport("nds_ds")]
 //extern static int db_parser_init(string db_root_path, string log_path, string citycode);
@@ -31,24 +33,126 @@
 
 
 typedef int (*db_parser_init)(const char* db_root_path, const char* log_path, const char* citycode);
+typedef int (*db_parser_get_routing_shape_points_serialize)(int tile_id, byte* road_shape_points);
+struct coordinate
+{
+	float x;
+	float y;
+};
+
 extern "C"
 {
-	
+
 	MY_DLL_API int set_data(char* data_path)
 	{
-		HMODULE  g_hDll = LoadLibrary(L"nds_ds.dll");
+		std::string path = data_path;
+		std::wstring dll_path = std::wstring(path.begin(), path.end()) + L"\\nds_ds.dll";
+		HMODULE  g_hDll = LoadLibraryEx(dll_path.c_str(), NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
 		if (g_hDll)
 		{
-			std::string path = data_path;
-			std::string log_path = path + "\\log2022\\";
-			db_parser_init func = (db_parser_init)GetProcAddress(g_hDll, "db_parser_init");
-			auto res = func(path.c_str(), log_path.c_str(), "1100");
+			{
+				std::string path = data_path;
+				std::string log_path = path + "\\log2022\\";
+				db_parser_init func = (db_parser_init)GetProcAddress(g_hDll, "db_parser_init");
+				auto res = func(path.c_str(), log_path.c_str(), "1100");
 
-			std::cout << "db_parser_init:  " << res << std::endl;
+				std::cout << "db_parser_init:  " << res << std::endl;
+				if (res != 0)
+					return 250;
+			}
+
+			{
+				const size_t size = 50000000;
+				thread_local byte* data = new byte[size]();
+				memset(data, 0, size);
+				db_parser_get_routing_shape_points_serialize func = (db_parser_get_routing_shape_points_serialize)GetProcAddress(g_hDll, "db_parser_get_routing_shape_points_serialize");
+				auto res = func(557467546, data);
+				if (res != 0)
+					return 251;
+
+				//#define cJSON_False 0
+				//#define cJSON_True 1
+				//#define cJSON_NULL 2
+				//#define cJSON_Number 3
+				//#define cJSON_String 4
+				//#define cJSON_Array 5
+				//#define cJSON_Object 6
+				//
+				//#define cJSON_IsReference 256
+
+								/*{
+									"coordinates": [
+										[
+										{
+											"x":116.47691666666667,
+												"y" : 39.98085069444444
+										},
+							{
+								"x":116.47611805555556,
+								"y" : 39.981378472222225
+							}
+										],
+											[
+											{
+												"x":116.46579166666666,
+													"y" : 39.988197916666664
+											},
+							{
+								"x":116.46588194444445,
+								"y" : 39.98828819444444
+							}
+											],*/
+
+				/*thread_local std::vector<coordinate> shapes;
+				shapes.clear();*/
+
+				cJSON* root = cJSON_Parse((const char*)data);
+				if (!root)
+					return 252;
+
+				cJSON* cur = root->child->child;
+
+				int cnt = root->child->next->valueint;
+
+				coordinate* shapes = new coordinate[cnt]();
+				int i = 0;
+
+				coordinate coor_p = { 0 };
+				while (cur)
+				{
+					switch (cur->type)
+					{
+					case cJSON_Array:
+					{
+						if (cur->child)
+						{
+							cJSON* mv_file_item = cur->child->child;
+							while (mv_file_item)
+							{
+								if ((strcmp("x", mv_file_item->string) == 0))
+								{
+									coor_p.x = (float)mv_file_item->valuedouble;
+								}
+								else
+								{
+									coor_p.y = (float)mv_file_item->valuedouble;
+
+									shapes[i++] = (coor_p);
+								}
+								mv_file_item = mv_file_item->next;
+							}
+						}
+					}
+					}
+					cur = cur->next;
+				}
+
+				std::cout << "point cnt: " << cnt << std::endl;
+			}
 		}
 
 
 		std::cout << data_path << std::endl;
-		return 100;
+		return 0;
 	}
 }
